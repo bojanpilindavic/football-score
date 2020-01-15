33915fe2dc4a135dea1fed49889315b441c18ed8
bojan.pilindavic <57361256+bojanpilindavic@users.noreply.github.com>
2020-01-15 17:17:31 +0100

    Updates

---
+++ precache-manifest.c93a42b3d5bbd73adb19b2eb26f9808d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "588553da0650f160c6661b46fd2b6b77",
+    "url": "/football-score/index.html"
+  },
+  {
+    "revision": "e65e765be520b2912ece",
+    "url": "/football-score/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "06b41493e7832b5ee098",
+    "url": "/football-score/static/css/main.6b0f7204.chunk.css"
+  },
+  {
+    "revision": "e65e765be520b2912ece",
+    "url": "/football-score/static/js/2.e95b768f.chunk.js"
+  },
+  {
+    "revision": "87984cad984dae3222b2a1d0861003a4",
+    "url": "/football-score/static/js/2.e95b768f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "06b41493e7832b5ee098",
+    "url": "/football-score/static/js/main.9f3c25ab.chunk.js"
+  },
+  {
+    "revision": "a2839205268efd0e0c78",
+    "url": "/football-score/static/js/runtime-main.7f6affdd.js"
+  },
+  {
+    "revision": "9e0d83711860fed0fa72f52dfead22c7",
+    "url": "/football-score/static/media/background.9e0d8371.jpg"
+  }
+]);
